099f3ca55b754f44d4b4f6c62f93a1aab30c12e2
oyo-112 <yohei212@gmail.com>
2022-02-09 15:04:42 +0900

    ユーザー名のカナ入力設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :family_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}, presence: true
+  validates :family_name_kana, format: {with: /\A[ァ-ヶー]+\z/}, presence: true
+  validates :given_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}, presence: true
+  validates :given_name_kana, format: {with: /\A[ァ-ヶー]+\z/}, presence: true
+  validates :birthday, presence: true
 end
