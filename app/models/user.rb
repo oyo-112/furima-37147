537b4bd9947e5b454d72aa92c9d319a6a0303260
oyo-112 <yohei212@gmail.com>
2022-02-27 13:16:22 +0900

    異常系テスト追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: PASSWORD_REGEX , message: "is invalid. Include both letters and numbers" }
   validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }, presence: true
-  validates :family_name_kana, format: { with: /\A[ァ-ヶー]+\z/ , message: "is invalid. Input full-width characters" }, presence: true
+  validates :family_name_kana, format: { with: /\A[ァ-ヶー]+\z/ , message: "is invalid" }, presence: true
   validates :given_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }, presence: true
-  validates :given_name_kana, format: { with: /\A[ァ-ヶー]+\z/ , message: "is invalid. Input full-width characters" }, presence: true
+  validates :given_name_kana, format: { with: /\A[ァ-ヶー]+\z/ , message: "is invalid" }, presence: true
   validates :birthday, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,7 +31,19 @@ RSpec.describe User, type: :model do
         @user.password = '111111'
         @user.password_confirmation = '111111'
         @user.valid?
-        expect(@user.errors.full_messages).to include('Password is invalid')
+        expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
+      end
+      it '英字のみのパスワードでは登録できない' do
+        @user.password = 'aaaaaa'
+        @user.password_confirmation = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
+      end
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = '１１１１１１'
+        @user.password_confirmation = '１１１１１１'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
       end
       it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password_confirmation = ''
@@ -54,6 +66,51 @@ RSpec.describe User, type: :model do
         @user.password_confirmation = '00000'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+      it '姓（全角）が空だと登録できない' do
+        @user.family_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name can't be blank")
+      end
+      it '姓（全角）に半角文字が含まれていると登録できない' do
+        @user.family_name_kana = 'a'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Family name kana is invalid')
+      end
+      it '名（全角）が空だと登録できない' do
+        @user.given_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Given name can't be blank")
+      end
+      it '名（全角）に半角文字が含まれていると登録できない' do
+        @user.given_name_kana = 'a'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Given name kana is invalid')
+      end
+      it '姓（カナ）が空だと登録できない' do
+        @user.family_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name kana can't be blank")
+      end
+      it '姓（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+        @user.family_name_kana = 'あ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Family name kana is invalid')
+      end
+      it '名（カナ）が空だと登録できない' do
+        @user.given_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Given name kana can't be blank")
+      end
+      it '名（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+        @user.given_name_kana = 'あ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Given name kana is invalid')
+      end
+      it '生年月日が空だと登録できない' do
+        @user.birthday = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
       end
     end
   end
