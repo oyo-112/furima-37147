78d953e0edea57eb9ce5a6bee7fb7d54c3df6dbd
oyo-112 <yohei212@gmail.com>
2022-03-06 16:43:50 +0900

    ストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :description, :price, :condition_id, :shipping_cost_id, :shipping_day_id, :prefecture_id, :category_id, :user)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase_record
+
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :condition_id, presence:true
+  validates :shipping_cost_id, presence: true
+  validates :shipping_day_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :category_id, presence: true
+  validates :user, presence: true
 end
