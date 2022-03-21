d568302174e39a2ed7c1811c15612aeba4dba52b
oyo-112 <yohei212@gmail.com>
2022-03-21 17:21:16 +0900

    テストコード作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,8 @@ class Item < ApplicationRecord
   has_one :purchase_record
 
   validates :image, presence: true
-  validates :item_name, presence: true
-  validates :description, presence: true
+  validates :item_name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
   validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, presence:true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_cost_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
