816c2d49058406d239881554ec00c81b047c9c68
oyo-112 <yohei212@gmail.com>
2022-03-23 15:32:10 +0900

    params修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,6 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :item_name, :description, :price, :condition_id, :shipping_cost_id, :shipping_day_id,
-                                 :prefecture_id, :category_id, :user).merge(user_id: current_user.id)
+                                 :prefecture_id, :category_id).merge(user_id: current_user.id)
   end
 end
