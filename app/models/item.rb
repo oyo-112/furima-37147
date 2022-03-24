bbb0bb6e44ce204a54d269892b170804f82b4b34
oyo-112 <yohei212@gmail.com>
2022-03-24 12:57:52 +0900

    アソシエーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,11 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  belongs_to :category
+  belongs_to :condition
   belongs_to :shipping_cost
-
-
-
-  
+  belongs_to :prefecture
+  belongs_to :shipping_day
   has_one_attached :image
   # has_one :purchase_record
 
