a533cdc19f6c2e1fda1ccb18afc76969c9da328f
oyo-112 <yohei212@gmail.com>
2022-03-31 11:59:23 +0900

    purchase_recordモデル作成

--- app/models/purchase_records_delivery_addresses.rb
+++ app/models/purchase_records_delivery_addresses.rb
@@ -1,4 +1,4 @@
-class OrderAddress
+class Purchase_recordsDelivery_addresses
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :order, :token
 
@@ -18,7 +18,7 @@ class OrderAddress
   end
 
   def save
-    order = Order.create(user_id: user_id, item_id: item_id)
+    purchase_record = Purchase_record.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
   
