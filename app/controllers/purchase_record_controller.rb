824de78778515fe1b62cdf0de2fd4dc8c504d87e
oyo-112 <yohei212@gmail.com>
2022-03-31 16:40:22 +0900

    フォームの実装など

--- app/controllers/purchase_record_controller.rb
+++ app/controllers/purchase_record_controller.rb
@@ -2,4 +2,8 @@ class PurchaseRecordController < ApplicationController
   def index
     @purchase_record_delivery_address = Purchase_recordsDelivery_addresses.new
   end
+
+  def create
+  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items
+  resources :items do
+    resources :purchase_records_delivery_addresses, only: :index
+  end
 end
