b283bcd1f637a0d76ed686fcc4120b5badf0deab
oyo-112 <yohei212@gmail.com>
2022-03-31 11:41:04 +0900

    formオブジェクト

---
+++ app/models/order_address.rb
@@ -0,0 +1,25 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :order, :token
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id
+    validates :city
+    validates :address
+    validates :phone_number, numericality: { only_integer: true }
+    validates :token
+    validates :user_id
+    validates :item_id
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :prefecture_id
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
+  
+end
